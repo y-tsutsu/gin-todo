425a9b8472000f3db848fb24add70c0f18901386
tsutsu <tsutsu0928@gmail.com>
2019-03-23 19:55:58 +0900

    GinのHello world．

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	router := gin.Default()
+	router.LoadHTMLGlob("templates/*.html")
+
+	data := "Hello, Go/Gin!!"
+
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.HTML(200, "index.html", gin.H{"data": data})
+	})
+
+	router.Run()
 }
