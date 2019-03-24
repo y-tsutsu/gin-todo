faaaa1591a0aa5e850ad982e96b9146072c110a8
tsutsu <tsutsu0928@gmail.com>
2019-03-25 00:05:28 +0900

    dbの操作を実装．

--- main.go
+++ main.go
@@ -2,7 +2,80 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/mattn/go-sqlite3"
 )
+
+// Todo ToDoモデル
+type Todo struct {
+	gorm.Model
+	Text   string
+	Status string
+}
+
+func dbInit() {
+	db, err := gorm.Open("sqlite3", "test.sqlite3")
+	if err != nil {
+		panic("Error (dbInit)")
+	}
+	db.AutoMigrate(&Todo{})
+	defer db.Close()
+}
+
+func dbInsert(text string, status string) {
+	db, err := gorm.Open("sqlite3", "test.sqlite3")
+	if err != nil {
+		panic("Error (dbInsert)")
+	}
+	db.Create(&Todo{Text: text, Status: status})
+	defer db.Close()
+}
+
+func dbUpdate(id int, text string, status string) {
+	db, err := gorm.Open("sqlite3", "test.sqlite3")
+	if err != nil {
+		panic("Error (dbInsert)")
+	}
+	var todo Todo
+	db.First(&todo, id)
+	todo.Text = text
+	todo.Status = status
+	db.Save(&todo)
+	defer db.Close()
+}
+
+func dbDelete(id int) {
+	db, err := gorm.Open("sqlite3", "test.sqlite3")
+	if err != nil {
+		panic("Error (dbInsert)")
+	}
+	var todo Todo
+	db.Find(&todo)
+	db.Delete(&todo)
+	defer db.Close()
+}
+
+func dbGetAll() []Todo {
+	db, err := gorm.Open("sqlite3", "test.sqlite3")
+	if err != nil {
+		panic("Error (dbInsert)")
+	}
+	var todos []Todo
+	db.Order("created_at desc").Find(&todos)
+	defer db.Close()
+	return todos
+}
+
+func dbGetOne(id int) Todo {
+	db, err := gorm.Open("sqlite3", "test.sqlite3")
+	if err != nil {
+		panic("Error (dbInsert)")
+	}
+	var todo Todo
+	db.First(&todo, id)
+	defer db.Close()
+	return todo
+}
 
 func main() {
 	router := gin.Default()
